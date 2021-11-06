9eed760c909efaab971bd5f98bfd5b569ef389af
Sebabin <sebastiancrespin279@gmail.com>
2021-11-05 19:54:53 -0600

    form complete and resets upon submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,12 @@ var taskFormHandler = function(event) {
     event.preventDefault();
 var taskNameInput = document.querySelector("input[name='task-name']").value;
 var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+if (!taskNameInput || !taskTypeInput) {
+    alert("you need to fill out the task form");
+    return false;
+}
+formEl.reset();
 // make data a new object
 var taskDataObj = {
     name: taskNameInput,
